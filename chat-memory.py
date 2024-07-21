54f5f43e12bc1245879062be05ea6c20a2f5d063
GauravN0910 <root@Gaurav>
2024-07-21 13:08:48 +0530

    Added context from history for Chat Engine

--- chat-memory.py
+++ chat-memory.py
@@ -16,8 +16,25 @@ embed_model = TogetherEmbedding(
     model_name = "togethercomputer/m2-bert-80M-8k-retrieval", api_key="dae9b33beef8cc57bc9475d53519e9e70037e6820b8c2d54b0b2f349e11101b0"
 )
 
+documents = SimpleDirectoryReader(input_dir="./data", recursive=True).load_data()
+vector_store = MilvusVectorStore(uri="./milvus_demo.db", dim=768, overwrite=True)
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
 Settings.embed_model = embed_model
 Settings.llm = llm
+
+promptTemplate = (
+    "We have provided context information below. \n"
+    "You are expected to provide appropriate over the counter medications to patients. \n"
+    "---------------------\n"
+    "{context_str}"
+    "\n---------------------\n"
+    "Given this information, please answer the question: {query_str}\n"
+    "If the question is out of the scope of the given information, answer the questions to the best of your ability \n"
+    "Do not mention details about the context provided to you and directly answer the question.\n"
+)
+
+qa_template = PromptTemplate(promptTemplate)
 
 
 try:
@@ -29,7 +46,7 @@ except FileNotFoundError:
 
 
 memory = ChatMemoryBuffer.from_defaults(
-    token_limit=2000,
+    token_limit=8000,
     chat_store=chat_store,
     chat_store_key="user_X"
     )  
@@ -40,7 +57,8 @@ while True:
     if user_message.lower() == 'exit':
         print("Exiting chat...")
         break
-    response = chat_engine.chat(user_message)
+    prompt = qa_template.format(context_str = documents, query_str = user_message)
+    response = chat_engine.chat(prompt)
     print(f"Chatbot: {response}")
 
 chat_store.persist(persist_path="chat_memory.json")
